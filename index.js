4884bf05d8e2fc0be8ac085546cea66b98d72f48
MANOJGUJJALA <balu88861@gmail.com>
2021-04-07 13:59:09 +0530

    manhdjyh

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ io.on('connection',(socket)=>{
 
     socket.on('disconnect',()=>{
         socket.broadcast.emit("callEnded")
-    })
+    }) 
 
     socket.on("calluser",({userToCall,signalData,from,name})=>{
 
