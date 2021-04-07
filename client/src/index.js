9a1733faed3deea9967ffb22a4f441ad8a957173
MANOJGUJJALA <balu88861@gmail.com>
2021-04-07 13:46:59 +0530

    pushing github

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import ReactDom from 'react-dom'
+
+import App from './App'
+
+import './styles.css'
+
+import {ContextProvider} from './SocketContext' 
+
+ReactDom.render(
+    <ContextProvider>
+
+      <App/>
+    </ContextProvider>
+,
+document.getElementById('root'))
